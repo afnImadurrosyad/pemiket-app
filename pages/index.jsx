09205a7160ef64e667cf6fef1d8b50a9d112da94
Afnnimadurrosyad <afnanimadurrosyad@gmail.com>
2025-03-07 14:39:23 +0700

    speed insight - vercel

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,10 +5,12 @@ import HeroSection from '../components/HeroSection';
 import Feedback from '../components/Feedback';
 import Rundown from './../components/Rundown';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function Home() {
   return (
     <div className='overflow-x-hidden'>
+      <SpeedInsights />
       <Analytics />
       <Navbar />
       <HeroSection />
